808f6fec3bcef73a50b225478472fe8f62f22802
Deniz Orsel <DenizOrsel@users.noreply.github.com>
2019-04-19 15:23:32 +0200

    Update sortNumberArray.js

--- sortNumberArray.js
+++ sortNumberArray.js
@@ -6,3 +6,21 @@ console.log (newArray.sort(function(a, b){
   return a-b;
 }
 ));
+
+/*
+var array = [110, 20, 40];
+var numberSortAsc = function(a,b){
+  return a-b;
+}
+var numberSortDes = function(a,b){
+  return b-a;
+}
+
+
+
+array.sort(numberSortDes);
+
+for (var i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+*/
